8c343ed9bc2ce1050d0dd4942246ad613418ba09
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-05-06 17:52:09 -0400

    structured html, added buttons and styling in css, added algorithm to script

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//1 enable search and link to each city
+
+//2 search history using local storage
+
+//3 display weather conditions in the dashboard for chosen city, showing  city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed.
+
+//4 display below chosen city 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+//5 when city in search history is clicked displays current and future conditions for that city 
