390da910e30a1433f4b9fb0ba974036f4264a3f5
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-05-08 22:02:11 -0400

    added comments and made changes to css

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//1 enable search and link to each city
+//1 enable search and link to each city -- open geo and weather apis
 
 //2 search history using local storage
 
@@ -7,3 +7,4 @@
 //4 display below chosen city 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 
 //5 when city in search history is clicked displays current and future conditions for that city 
+
